b8e910ec14f8fbca7ba349570b795617682ca791
Dan Calacci <dan@dcalacci.net>
2014-10-01 20:03:56 -0400

    added similarity functions that use numpy!
    fft, get_mono, downsample, etc.
    
    Use similarity(fpath1, fpath2) to get the cosine similarity of the fft
    of two files.
    
    Unsure if cosine similarity is suitable for comparing two complex arrays...

---
+++ assn4/src/match.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.io import wavfile
+from scipy.signal import decimate
+from scipy.spatial import distance
+import datetime
+
+def downsample(stream):
+    """ Downsample from 44100 -> 5512Hz
+    """
+    return decimate(stream, 8)
+
+def get_mono(fpath):
+    """ Converts the given wav file to 5512Hz PCM Mono
+    """
+    print datetime.datetime.now(), "reading file..."
+    samplerate, data = wavfile.read(fpath)
+    #print datetime.datetime.now(), "downsampling..."
+    #channels = map(downsample, data)
+    channels = data
+    print datetime.datetime.now(), "converting to mono..."
+    return list(np.mean(t) for t in zip(channels))
+
+def fft(fpath):
+    mono_stream = get_mono(fpath)
+    print datetime.datetime.now(), "getting fft..."
+    return np.fft.fft(mono_stream)
+
+def mse(A, B):
+    dist = [(a - b) ** 2 for a, b in zip(A, B)]
+    return np.mean(dist)
+
+def similarity(f1, f2):
+    ffts = map(fft, [f1, f2])
+    print datetime.datetime.now(), "computing mse..."
+    return distance.cosine(ffts[0], ffts[1])
