e25743a0c8e0b0233cae62a3427b9ad20b8f4c93
tswartz <tswartz92@gmail.com>
2014-10-06 23:04:00 -0400

    Added is_match tests, takes over 20 mins

--- assn4/src/tests.py
+++ assn4/src/tests.py
@@ -5,25 +5,51 @@ import match
 
 class TestAudioMatchingFunctions(unittest.TestCase):
 
-	def test_mse(self):
-		self.run_mse_test("../data/z01.wav", "../data/z02.wav", 495200)
+	def test_is_match(self):
+		print "Sit back and relax, this may take a while..."
 
-	# running single mse_test
-	def run_mse_test(self, true_audio, suspect_audio, expected_mse):
+		# Should be matches
+		self.run_is_match_test("../data/z01.wav", "../data/z02.wav", True)
+		self.run_is_match_test("../data/z03.wav", "../data/z04.wav", True)
+		self.run_is_match_test("../data/z05.wav", "../data/z06.wav", True)
+		self.run_is_match_test("../data/z07.wav", "../data/z08.wav", True)
+		self.run_is_match_test("../data/z07.wav", "../data/Sor4959.wav", True)
+		self.run_is_match_test("../data/z05.wav", "../data/Sor1929.wav", True)
+		self.run_is_match_test("../data/z03.wav", "../data/bad2131.wav", True)
+		self.run_is_match_test("../data/z02.wav", "../data/bad0616.wav", True)
+
+		# Should NOT be matches
+		self.run_is_match_test("../data/z02.wav", "../data/z03.wav", False)
+		self.run_is_match_test("../data/z04.wav", "../data/z05.wav", False)
+		self.run_is_match_test("../data/z06.wav", "../data/z07.wav", False)
+		self.run_is_match_test("../data/z05.wav", "../data/Sor5308.wav", False)
+		self.run_is_match_test("../data/Sor4959.wav", "../data/Sor1929.wav", False)
+		self.run_is_match_test("../data/z02.wav", "../data/bad2131.wav", False)
+		self.run_is_match_test("../data/z03.wav", "../data/bad0616.wav", False)
+
+		# test mp3s, nonexistent files!!
+
+	# running single is_match test
+	def run_is_match_test(self, true_audio, suspect_audio, expected_is_match):
+		if (expected_is_match):
+			print "Testing that ", true_audio," and ", suspect_audio," match..."
+		else:
+			print "Testing that ", true_audio," and ", suspect_audio," DO NOT match..."
+
 		# prevents printing
 		sys.stdout = open(os.devnull, "w")
 
-		actual_mse = match.similarity(true_audio, suspect_audio)
-		actual_mse = int(actual_mse)
+		actual_is_match = match.is_match(true_audio, suspect_audio)
 
 		# ok now you can print again
 		sys.stdout = sys.__stdout__
 
-		self.assertEqual(expected_mse, actual_mse)
+		if (expected_is_match):
+			self.assertTrue(actual_is_match)
+		else:
+			self.assertFalse(actual_is_match)
+		print "Test passed!"
 
 
 # calls all test_* functions in TestAudioMatchingFunctions
-def run_tests():
-	unittest.main()
-
-run_tests()
+unittest.main()
