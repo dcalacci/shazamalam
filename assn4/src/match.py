134a03aaf905cf602d47f681c551bed4e6bf3119
Dan Calacci <dan@dcalacci.net>
2014-10-10 20:53:24 -0400

    2.4s for all tests (way way faster)
    I optimized a bunch of functions using numpy, but made some sacrifices:
    
    1. we're now only using the real parts of the fft output
    
    2. The threshold changed accordingly
    
    3. Why were we reading in the sample using read_wav instead of
    numpy (get_mono) in is_match?
    
    4. Why were we reading in the samples to begin with in is_match? What
    did dividing the coefficient by the sample array do, and why did that
    return a number instead of an array? (I didn't play around with the code
    much, I just hacked at it)
    
    Anyway its a million times faster now and still works I think

--- assn4/src/match.py
+++ assn4/src/match.py
@@ -13,51 +13,37 @@ def downsample(stream):
 def get_mono(fpath):
     """ Converts the given wav file to 5512Hz PCM Mono
     """
-    #print datetime.datetime.now(), "reading file..."
     samplerate, channels = wavfile.read(fpath)
-    #print datetime.datetime.now(), "downsampling..."
-    #channels = map(downsample, data)
-    #print datetime.datetime.now(), "converting to mono..."
     return np.mean(channels, axis=1)
 
 def fft(fpath):
     mono_stream = get_mono(fpath)
-    #print datetime.datetime.now(), "getting fft..."
     return np.fft.fft(mono_stream)
 
 def mse(A, B):
-    #convert from complex to real vectors
-    A = map(convert, A)
-    B = map(convert, B)
-    #caluclate euclidean distance between A and B
-    dist = [distance.euclidean(a,b) ** 2 for a, b in zip(A, B)]
-    return np.mean(dist)
-
-def convert(complx):
-    return [complx.real, complx.imag]
+    return ((np.real(A) - np.real(B)) ** 2).mean()
 
 def similarity(f1, f2):
     ffts = map(fft, [f1, f2])
-    #print datetime.datetime.now(), "computing mse..."
     return mse(ffts[0], ffts[1])
 
 """
-is_match: 
+is_match:
 Returns a boolean if we deem that f1 and f2 match
 
 INPUT: 2 files that may or MAY NOT be valid file paths
 OUTPUT: True if we deem the files match, otherwise False
 """
 def is_match(f1, f2):
-    
+
     #preapre a string for our output
-    match_threshold = 10000000 #this is our trial and error threshold
+    match_threshold = 150000000000 # new threshold from new trial and error
     match_coefficient = 0
 
     #validate that these audio files are legit
     if (not (read_wav.validate_file(f1) and read_wav.validate_file(f2))):
         return False #validate file will handle error messages if validation fails
-    
+
     """
     validate that the two files are of the same length
     """
@@ -65,12 +51,8 @@ def is_match(f1, f2):
     if(read_wav.length(f1) != read_wav.length(f2)):
         return False
     else:
-        #now lets read those wave files
-        true_signal, true_sample = read_wav.mono_channel(f1)
-        suspect_signal, suspect_sample = read_wav.mono_channel(f2)
-
-        #look at the fft, oh my theres a lot of info huh
-        match_coefficient = similarity(f1, f2) / true_sample
+        # get our match coefficient!
+        match_coefficient = similarity(f1, f2)
 
     #final print out to SDTOUT
     if(match_coefficient < match_threshold):
